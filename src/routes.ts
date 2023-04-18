1aa952c604968ec58142c74a456967950a6e872d
Capt. Michael <XinwenCheng@Live.com>
2023-04-18 12:32:49 +0800

    feat: add user profile

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import { NotificationRouteType } from './type/notification.type';
 import { TemplateRouteType } from './type/template.type';
-import { UserRouteType } from './type/user.type';
+import { UserProfileRouteType, UserRouteType } from './type/user.type';
 
 export class ApiRoute {
   static User: UserRouteType = { path: '/users', get: { username: '' }, post: { username: '', nickname: '' } };
+  static UserProfile: UserProfileRouteType = {
+    path: '/user/profile',
+    get: { username: '' },
+    post: { userId: '' }
+  };
   static Notification: NotificationRouteType = { path: '/notifications', get: { pageIndex: 0, pageSize: 20 } };
   static Template: TemplateRouteType = { path: '/templates', get: { pageIndex: 0, pageSize: 20 } };
   static TemplateLabel: TemplateRouteType = { path: '/templates/labels', get: { pageIndex: 0, pageSize: 500 } };
--- src/type/user.type.ts
+++ src/type/user.type.ts
@@ -3,3 +3,18 @@ import { RouteType } from './route.type';
 export type UserRouteType = RouteType & { get: { username: string } } & {
   post: { username: string; nickname?: string };
 };
+
+export type UserProfileRouteType = RouteType & { get: { username: string } } & {
+  post: {
+    id?: string;
+    userId: string;
+    location?: string;
+    occupation?: string;
+    about?: string;
+    twitterUsername?: string;
+    facebookUsername?: string;
+    linkedInUsername?: string;
+    instagramUsername?: string;
+    website?: string;
+  };
+};
