4decc5e880a26fee8f933024bf8474ae59dd2197
Capt. Michael <XinwenCheng@Live.com>
2023-04-22 00:40:54 +0800

    perf: improve task attachment type for downloading

--- src/type/task.type.ts
+++ src/type/task.type.ts
@@ -1,6 +1,6 @@
 import { RouteType } from './route.type';
 
 export type TaskAttachmentType = RouteType & {
-  get: { taskId?: string; storageFilename?: string };
+  get: { taskId?: string; storageFilename?: string; isDownload?: boolean };
   deletion: { attachmentId: string; storageFilename: string };
 };
