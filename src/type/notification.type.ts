e5117b5de238d5ae5dcf4355c24817df4a7c6d30
Capt. Michael <XinwenCheng@Live.com>
2023-05-01 18:20:26 +0800

    feat: route types of notification

--- src/type/notification.type.ts
+++ src/type/notification.type.ts
@@ -1,3 +1,8 @@
+import { NotificationDataType } from '@clearmylist/data-types';
 import { RouteType } from './route.type';
 
-export type NotificationRouteType = RouteType & { get: { userId: string; pageIndex: number; pageSize: number } };
+export type NotificationRouteType = RouteType & { get: NotificationGetRequestType };
+
+export type NotificationGetRequestType = { userId: string; pageIndex: number; pageSize: number };
+
+export type NotificationGetResponseType = { notifications: NotificationDataType[] };
