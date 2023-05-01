ebdd81f8d97df346a64d0f3493cd7d0d7657ba6c
Capt. Michael <XinwenCheng@Live.com>
2023-05-01 18:33:58 +0800

    perf: notification get list response type

--- src/type/notification.type.ts
+++ src/type/notification.type.ts
@@ -1,8 +1,9 @@
 import { NotificationDataType } from '@clearmylist/data-types';
 import { RouteType } from './route.type';
+import { BaseResponseType } from './base.type';
 
 export type NotificationRouteType = RouteType & { get: NotificationGetRequestType };
 
 export type NotificationGetRequestType = { subscriberId: string; pageIndex: number; pageSize: number };
 
-export type NotificationGetResponseType = { notifications: NotificationDataType[] };
+export type NotificationGetResponseType = BaseResponseType & { notifications: NotificationDataType[] };
