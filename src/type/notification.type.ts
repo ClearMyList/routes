bc8623fc1aa2a1004a03bb90562b86ae8529f3c6
Capt. Michael <XinwenCheng@Live.com>
2023-05-02 12:25:16 +0800

    refactor: rename notification get route types to notification get list

--- src/type/notification.type.ts
+++ src/type/notification.type.ts
@@ -2,11 +2,11 @@ import { NotificationDataType } from '@clearmylist/data-types';
 import { RouteType } from './route.type';
 import { BaseResponseType } from './base.type';
 
-export type NotificationRouteType = RouteType & { get: NotificationGetRequestType };
+export type NotificationRouteType = RouteType & { get: NotificationGetListRequestType };
 
-export type NotificationGetRequestType = { subscriberId: string; pageIndex: number; pageSize: number };
+export type NotificationGetListRequestType = { subscriberId: string; pageIndex: number; pageSize: number };
 
-export type NotificationGetResponseType = BaseResponseType & { notifications: NotificationDataType[] };
+export type NotificationGetListResponseType = BaseResponseType & { notifications: NotificationDataType[] };
 
 export type NotificationUnreadCountRouteType = RouteType & { get: NotificationUnreadCountGetRequestType };
 
