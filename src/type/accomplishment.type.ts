cde368cd6a3ef4c89a6466337865b40a2b2a3235
Capt. Michael <XinwenCheng@Live.com>
2023-05-04 14:22:44 +0800

    perf: route types of accomplishment

--- src/type/accomplishment.type.ts
+++ src/type/accomplishment.type.ts
@@ -1,5 +1,9 @@
+import { AccomplishmentDataType } from '@clearmylist/data-types';
+import { BaseResponseType } from './base.type';
 import { RouteType } from './route.type';
 
 export type AccomplishmentRouteType = RouteType & { get: AccomplishmentGetRequestType };
 
 export type AccomplishmentGetRequestType = { username: string; year?: number };
+
+export type AccomplishmentGetResponseType = BaseResponseType & { accomplishment: AccomplishmentDataType };
