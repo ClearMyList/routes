c9e1f8c35f72c9f3e5165827bfc372ba5502864e
Capt. Michael <XinwenCheng@Live.com>
2023-06-02 17:44:28 +0800

    feat: route types of quota

--- src/type/quota.type.ts
+++ src/type/quota.type.ts
@@ -1,7 +1,24 @@
 import { QuotaDataType } from '@clearmylist/data-types';
-import { BaseRouteType } from './base.type';
+import { BaseRequestType, BaseResponseType, BaseRouteType } from './base.type';
 
 export interface QuotaRouteType extends BaseRouteType {
-  get: { username: string };
-  post: { username: string; quota: QuotaDataType };
+  get: QuotaGetRequestType;
+  post: QuotaPostRequestType;
+}
+
+export interface QuotaGetRequestType extends BaseRequestType {
+  username: string;
+}
+
+export interface QuotaGetResponseType extends BaseResponseType {
+  quota?: QuotaDataType;
+}
+
+export interface QuotaPostRequestType extends BaseRequestType {
+  username: string;
+  quota: QuotaDataType;
+}
+
+export interface QuotaPostResponseType extends BaseResponseType {
+  quota: QuotaDataType;
 }
